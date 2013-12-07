3a4b87c77a6a4ae9d8424493f1faa44d57927da2
Garth Gaughan <garth0323@gmail.com>
2013-12-06 21:45:27 -0700

    add home page content

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Message::Application.routes.draw do
   devise_for :views
   root "pages#home"
   get "about" => "pages#about"
+  get "professionals" => "pages#professionals"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
